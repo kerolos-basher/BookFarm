510e5e961f8861e9deaf123a1bfce696dddcae82
Kerolos <44545152+KerolosGergs@users.noreply.github.com>
2025-01-04 17:55:04 +0200

    kero-4-1-2025NeedTotalPrice

--- BookFarmBE/BookFarm/Controllers/BookFarmController.cs
+++ BookFarmBE/BookFarm/Controllers/BookFarmController.cs
@@ -6,6 +6,7 @@ using BookFarm.Migrations;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Polly;
 
 namespace BookFarm.Controllers
 {
@@ -25,27 +26,56 @@ namespace BookFarm.Controllers
 
         public async Task<ActionResult<List<BookAFarm>>> GetALlBookFarm()
         {
-           var BookFarms = await _context.books.ToListAsync();
-           var confirmationCodes = await _context.ConfirmBook.ToListAsync();
+           //var BookFarms = await _context.books.ToListAsync();
+           //var confirmationCodes = await _context.ConfirmBook.ToListAsync();
+           //var places = await _context.places.ToListAsync();
 
       // Perform the left join
-      var leftJoinResult = from book in BookFarms
-                           join code in confirmationCodes
-                           on book.ConfirmCode equals code.ConfirmCode into codeGroup
-                           from subCode in codeGroup.DefaultIfEmpty() // Left Join
-                           select new
-                           {
-                             //just put all the data that you need here
-                             BookId = book.Id,
-                             BookName = book.Name,
-                             BookEmail = book.Email,
-                             ConfirmCode = book.ConfirmCode ?? "No Code",
-                             confirmimage = subCode.PicturePath ?? "",
-                           };
+      //var leftJoinResult = from book in BookFarms
+      //                     join code in confirmationCodes
+      //                     on book.ConfirmCode equals code.ConfirmCode into codeGroup
+      //                     from subCode in codeGroup.DefaultIfEmpty() // Left Join
+
+      //                     select new
+      //                     {
+      //                       //just put all the data that you need here
+      //                       BookId = book.Id,
+      //                       BookName = book.Name??"",
+      //                       BookEmail = book.Email??"",
+      //                       ConfirmCode = book.ConfirmCode ?? "No Code",
+      //                       confirmimage = subCode.PicturePath ?? "",
+      //                     };
+
+
+      var result = from book in _context.books
+                   join confirmBook in _context.ConfirmBook
+                   on book.ConfirmCode equals confirmBook.ConfirmCode into confirmGroup
+                   from confirm in confirmGroup.DefaultIfEmpty() // LEFT JOIN
+                   join place in _context.places
+                   on book.placeID equals place.Id into placeGroup
+                   from place in placeGroup.DefaultIfEmpty() // LEFT JOIN
+                   select new
+                   {
+                     BookId = book.Id,
+                     BookName = book.Name,
+                     BookEmail=book.Email,
+                     BookPhone = book.PhoneNumber,
+                     BookDateFrom = book.DateFrom,
+                     bookDateTo = book.DateTo,
+                     bookImage = book.PicturePath,
+                     ConfirmCode = book.ConfirmCode,
+                     confirmImage = confirm.PicturePath,
+                     placeId = place.Id,
+                     placeName = place.Name,
+                     placeDescription = place.Description,
+                     placePriceForNight = place.PriceForNight,
+
+
+                   };
 
 
 
-      return Ok(leftJoinResult);
+      return Ok(result);
         }
         [HttpGet("{id}")]
        
--- BookFarmFE/src/app/component/admin/admin.component.ts
+++ BookFarmFE/src/app/component/admin/admin.component.ts
@@ -44,15 +44,18 @@ export class AdminComponent implements OnInit {
       next: (data) => {
         // this.rooms = data;
         this.rooms = data.map((room) => ({
-          id: room.Id, // Extracting specific properties
-          Name: room.Name,
-          Email: room.Email,
-          PhoneNumber: room.PhoneNumber,
-          DateFrom: room.DateFrom,
-          DateTo: room.DateTo,
-          price: room.place?.PriceForNight || 0, // Default value if price is missing
-          description: room.place?.Description || 'No description available',
-          picturePath: 'https://api.liwa.my-alphatech.com'+ room.PicturePath , // Fallback image
+          BookId: room.BookId, // Extracting specific properties
+          BookName: room.BookName,
+          BookEmail: room.BookEmail,
+          BookPhone: room.BookPhone,
+          BookDateFrom: room.BookDateFrom,
+          bookDateTo: room.bookDateTo,
+          bookImage: environment.ImgUrl+ room.bookImage , // Fallback image
+          confirmImage: environment.ImgUrl+ room.confirmImage,
+          placeName : room.placeName,
+          placeDescription : room.placeDescription,
+          TotaPrice:room.TotaPrice
+          
         }));
       },
       error: (err) => {
@@ -64,9 +67,10 @@ export class AdminComponent implements OnInit {
 
   deleteRoom(roomId: number): void {
     debugger
-    this.http.delete(environment.apiUrl+`/DeleteBook/${roomId}`).subscribe({
+    this.http.get(environment.apiUrl+`/DeleteBook/${roomId}`).subscribe({
       next: () => {
         this.rooms = this.rooms.filter((room) => room.Id !== roomId);
+        this.fetchRooms();
       //  alert('Room deleted successfully.');
       },
       error: (err) => {
